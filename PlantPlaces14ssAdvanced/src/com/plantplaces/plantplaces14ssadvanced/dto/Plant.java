dda865e4adabcf8ff264356d83f1b2fae4d31da4
jonesbr <jonesbr@ucmail.uc.edu>
2014-01-16 19:39:08 -0500

    Create our Plant DTO

---
+++ PlantPlaces14ssAdvanced/src/com/plantplaces/plantplaces14ssadvanced/dto/Plant.java
@@ -0,0 +1,55 @@
+package com.plantplaces.plantplaces14ssadvanced.dto;
+
+public class Plant {
+
+	public String getGenus() {
+		return genus;
+	}
+	public void setGenus(String genus) {
+		this.genus = genus;
+	}
+	public String getSpecies() {
+		return species;
+	}
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+	public String getCultivar() {
+		return cultivar;
+	}
+	public void setCultivar(String cultivar) {
+		this.cultivar = cultivar;
+	}
+	public String getCommon() {
+		return common;
+	}
+	public void setCommon(String common) {
+		this.common = common;
+	}
+	public int getMaxHeight() {
+		return maxHeight;
+	}
+	public void setMaxHeight(int maxHeight) {
+		this.maxHeight = maxHeight;
+	}
+	public boolean isEdible() {
+		return edible;
+	}
+	public void setEdible(boolean edible) {
+		this.edible = edible;
+	}
+	public boolean isRecommended() {
+		return recommended;
+	}
+	public void setRecommended(boolean recommended) {
+		this.recommended = recommended;
+	}
+	String genus;
+	String species;
+	String cultivar;
+	String common;
+	int maxHeight;
+	boolean edible;
+	boolean recommended;
+	
+}
