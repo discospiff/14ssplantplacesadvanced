f1ed17b752b8a6f2aefa6193f0864d2224a47880
jonesbr <jonesbr@ucmail.uc.edu>
2014-01-16 19:49:12 -0500

    Show a toString of the plant.

--- PlantPlaces14ssAdvanced/src/com/plantplaces/plantplaces14ssadvanced/SearchPlantsActivity.java
+++ PlantPlaces14ssAdvanced/src/com/plantplaces/plantplaces14ssadvanced/SearchPlantsActivity.java
@@ -1,15 +1,36 @@
 package com.plantplaces.plantplaces14ssadvanced;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
+
+import com.plantplaces.plantplaces14ssadvanced.dto.Plant;
 
 public class SearchPlantsActivity extends Activity {
 
-    @Override
+    private TextView edtStatus;
+
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_plants);
+        // create a new plant.
+        Plant plant = new Plant();
+        
+        plant.setGenus("Cercis");
+        plant.setSpecies("canadensis");
+        plant.setCommon("Redbud");
+        
+        // get a reference to the status text
+        edtStatus = (TextView) findViewById(R.id.edtStatus);
+        
+        String plantStatus = plant.toString();
+        
+        // put this status in the edt.
+        edtStatus.setText(plantStatus);
+        
     }
 
 
