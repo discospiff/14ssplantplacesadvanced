8660375d91db5720c720f48f38a218bc96554cac
jonesbr <jonesbr@ucmail.uc.edu>
2014-01-16 19:13:30 -0500

    Initial commit after path fix

---
+++ PlantPlaces14ssAdvanced/src/com/plantplaces/plantplaces14ssadvanced/SearchPlantsActivity.java
@@ -0,0 +1,23 @@
+package com.plantplaces.plantplaces14ssadvanced;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SearchPlantsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_plants);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.search_plants, menu);
+        return true;
+    }
+    
+}
