520824ff2f9f0c6e08c65cb9d66098c4433d3a89
jonesbr <jonesbr@WINWCHAR-247-TC.ucloud.uc.edu>
2014-03-13 19:51:02 -0400

    Implement a callback from Fragment to Activity.

--- PlantPlaces14ssAdvanced/src/com/plantplaces/plantplaces14ssadvanced/MasterDetailActivity.java
+++ PlantPlaces14ssAdvanced/src/com/plantplaces/plantplaces14ssadvanced/MasterDetailActivity.java
@@ -3,7 +3,7 @@ package com.plantplaces.plantplaces14ssadvanced;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
-public class MasterDetailActivity extends FragmentActivity {
+public class MasterDetailActivity extends FragmentActivity implements PlantSearchFragment.CoordinateSearch {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +11,12 @@ public class MasterDetailActivity extends FragmentActivity {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.plantmasterdetail);
+	}
+
+	@Override
+	public void search(String searchTerm) {
+		// TODO Auto-generated method stub
+		int i = 1 + 1;
 	}
 	
 }
--- PlantPlaces14ssAdvanced/src/com/plantplaces/plantplaces14ssadvanced/PlantSearchFragment.java
+++ PlantPlaces14ssAdvanced/src/com/plantplaces/plantplaces14ssadvanced/PlantSearchFragment.java
@@ -1,14 +1,20 @@
 package com.plantplaces.plantplaces14ssadvanced;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 
 public class PlantSearchFragment extends Fragment {
+
+	// declare our callback activity
+	private CoordinateSearch coordinateSearch;
+	private AutoCompleteTextView actSearchTerm;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,6 +27,8 @@ public class PlantSearchFragment extends Fragment {
 		// Get access to our button.
 		Button btnSearchFragment = (Button) layout.findViewById(R.id.btnSearchFragment);
 	
+		actSearchTerm = (AutoCompleteTextView) layout.findViewById(R.id.actSearchTerm);
+		
 //		// assign a button listener.
 //		btnSearchFragment.setOnClickListener(new OnClickListener() {
 //
@@ -40,17 +48,51 @@ public class PlantSearchFragment extends Fragment {
 		
 	}
 
+	
+	
 	class JeremysOnClickListener implements OnClickListener {
 
 		@Override
 		public void onClick(View v) {
-			// TODO Auto-generated method stub
-			int i = 1 + 1;
+			// get the search term from the auto complete.
+			String searchTerm = actSearchTerm.getText().toString();
+			
+			// invoke the callback method.
+			coordinateSearch.search(searchTerm);
 			
 		}
 		
 	}
 	
+	/**
+	 * Get a reference to the activity that will manage this fragment
+	 */
+	@Override
+	public void onAttach(Activity activity) {
+		// assign our callback activity
+		coordinateSearch = (CoordinateSearch) activity;
+		
+		
+		// TODO Auto-generated method stub
+		super.onAttach(activity);
+	}
+	
+	
+	/**
+	 * Callback interface that allows two fragments to coordinate a search, by using an activity
+	 * @author jonesbr
+	 *
+	 */
+	interface CoordinateSearch {
+		
+		/**
+		 * This method will be invoked when we click search on one fragment.  
+		 * The method and interface should be implemented on the class that owns the fragments
+		 * @param searchTerm
+		 */
+		public void search(String searchTerm);
+		
+	}
 	
 	
 }
